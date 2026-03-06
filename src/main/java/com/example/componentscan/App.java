1dd6691b1b8194f87b665308d49e67f354ffeac6
ibrahimmasmanaci <ibrahimmasmanaci@gmail.com>
2026-03-06 19:44:48 +0300

    Configuration class is added, so from now on we don't need any configuration.xml file.

--- src/main/java/com/example/componentscan/App.java
+++ src/main/java/com/example/componentscan/App.java
@@ -1,13 +1,15 @@
 package com.example.componentscan;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationWithComponentAnnotation.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         Employee employee =  context.getBean("employee", Employee.class);
         System.out.println(employee);
     }
